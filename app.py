567c308f90aba83b170130a3eb1bd302eca3fae2
Akhlaqul Muhammad Fadwa <75623219+zennn08@users.noreply.github.com>
2025-12-13 20:05:12 +0700

    refactor: app.run to be under main guard

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ async def analyze():
     return jsonify(result), 200
 
 
-port = int(os.getenv("PORT", 5000))
-app.run(host="127.0.0.1", port=port, threaded=False)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="127.0.0.1", port=port, debug=False)
