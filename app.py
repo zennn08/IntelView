42fa2a7fb422a51e5f21eadb6f53193dc544c8c8
zennn08 <am.fadwa15@gmail.com>
2025-12-13 17:52:57 +0700

    fix: set host to local

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ async def analyze():
 
 
 port = int(os.getenv("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True, threaded=False)
+app.run(host="127.0.0.1", port=port, debug=True, threaded=False)
