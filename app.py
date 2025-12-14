464f00548c444a0285a50ecd9545ad65bd721640
Akhlaqul Muhammad Fadwa <75623219+zennn08@users.noreply.github.com>
2025-12-14 22:42:17 +0700

    fix: Change app host to '0.0.0.0' for external access

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ async def analyze():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="127.0.0.1", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
